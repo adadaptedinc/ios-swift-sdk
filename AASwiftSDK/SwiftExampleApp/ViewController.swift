ecae795e1986a0a1efb98161c9e14b7daab01469
Brett Clifton <bclifton@adadapted.com>
2020-08-10 16:03:17 -0400

    feat: adding base project files

---
+++ AASwiftSDK/SwiftExampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftExampleApp
+//
+//  Created by Brett Clifton on 8/10/20.
+//  Copyright © 2020 AdAdapted. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
