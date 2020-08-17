c94c516499a705a93c28678ef213385c880f3dad
Brett Clifton <bclifton@adadapted.com>
2020-08-17 12:17:30 -0400

    feat: adding file structure

---
+++ AASwiftSDK/InternalProtocols.swift
@@ -0,0 +1,10 @@
+//
+//  InternalProtocols.swift
+//  AASwiftSDK
+//
+//  Created by Brett Clifton on 8/12/20.
+//  Copyright © 2020 AdAdapted. All rights reserved.
+//
+
+import Foundation
+
---
+++ AASwiftSDK/Public/AASDK.swift
@@ -0,0 +1,9 @@
+//
+//  AASDK.swift
+//  AASwiftSDK
+//
+//  Created by Brett Clifton on 8/12/20.
+//  Copyright © 2020 AdAdapted. All rights reserved.
+//
+
+import Foundation
